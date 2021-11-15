934470c76594f6e2e867062a9b8ab5ff3f7ff99b
Einar Kjellback <einar.kjellback@avinor.no>
2021-11-15 18:05:24 +0100

    Buttons are constructed in main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 // Domain
-let stack = [];
+// let stack = [];
+
+// Controller
+let numButtons = [];
 
 // Layout HTML
+// Construct buttons
 let body = document.body;
-let testButton = document.createElement('button');
-let text = document.createTextNode('123');
-tag.appendChild(text);
-body.appendChild(testButton);
+for (var i = 0; i < 10; i++) {
+    let numButton = document.createElement('button');
+    let text = document.createTextNode(i);
+    numButton.appendChild(text);
+    numButton.type = 'display_on_press';
+    numButton.value = i;
+    body.appendChild(numButton);
 
+    numButtons.push(numButton);
+}
+
+// Display buttons
+for (var numButton of numButtons) {
+
+}
 
 // Controller/GUI
 let buttons = document.getElementsByTagName('button');
