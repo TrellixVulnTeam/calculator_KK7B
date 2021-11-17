7b93694df8480c3a0edb5262a2a73bb194195c3e
Einar Kjellback <einar.kjellback@avinor.no>
2021-11-17 14:15:12 +0100

    Calculator now handles addition with first number being negative

--- js/src/domain/calculator.mjs
+++ js/src/domain/calculator.mjs
@@ -13,26 +13,46 @@ const operator = {
 
 const Calculator = () => {
     let display = '';
-    let a = 0;
+    let a = undefined;
     const builder = NumberBuilder();
     let nextOperation = undefined;
+    let newExpression = true;
+
+    /* Decide if subtract button should negate next number or act as a subtraction in an expression. */
+    let negate = false;
 
     return {
         pressNumber: (n) => {
+            newExpression = false;
             display += n.toString();
             builder.add(n);
         },
         pressAdd: () => {
+            newExpression = false;
             display += ' + ';
             nextOperation = operator.ADD;
-            a = builder.buildPositive();
+            if (negate) {
+                a = builder.buildNegative();
+            } else {
+                a = builder.buildPositive();
+            }
         },
         pressSubtract: () => {
             display += ' - ';
-            nextOperation = operator.SUBTRACT;
-            a = builder.buildPositive();
+            if (newExpression) {
+                negate = true;
+            } else {
+                nextOperation = operator.SUBTRACT;
+                if (negate) {
+                    a = builder.buildNegative();
+                } else {
+                    a = builder.buildPositive();
+                }
+            }
+            newExpression = false;
         },
         pressEquals: () => {
+            newExpression = true;
             const b = builder.buildPositive();
             display = nextOperation(a, b).toString();
         },
