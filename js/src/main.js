31870d9586e9d2de9ded907ade036acc49a071fa
Einar Kjellback <einar.kjellback@avinor.no>
2021-11-17 11:32:48 +0100

    Deleted deadcode

--- js/src/main.js
+++ js/src/main.js
@@ -1,19 +1,7 @@
 import {
-    NumberBuilder
-} from './domain/number.mjs'
-
-import {
     Calculator
 } from './domain/calculator.mjs';
-
-// Domain
-let a;
-let b;
-let builder = NumberBuilder();
-
 const calculator = Calculator();
-
-let lastPressWasEquals = false;
 
 let numButtons = constructNumButtons();
 let addButton = constructButton('+');
@@ -44,55 +32,6 @@ for (let button of allButtons) {
     })
 }
 
-/*
-
-equalsButton.addEventListener('click', () => {
-    clearInput();
-});
-
-for (let button of allButtons) {
-    button.addEventListener('click', () => {
-        if (lastPressWasEquals) {
-            clearInput();
-        }
-    });
-}
-
-for (let button of numButtons.concat([addButton])) {
-    button.addEventListener('click', () => {
-        lastPressWasEquals = false;
-    });
-}
-
-// Add event listeners to buttons
-for (let button of allButtons) {
-    let v = button.value;
-    button.addEventListener("click", () => {
-        result.value += v;
-    });
-}
-
-addButton.addEventListener('click', () => {
-    a = builder.build().value;
-});
-
-equalsButton.addEventListener('click', () => {
-    lastPressWasEquals = true;
-    result.value += a + builder.build().value;
-});
-
-for (let button of numButtons) {
-    let v = button.value;
-    button.addEventListener('click', () => {
-        if (typeof a === 'undefined') {
-            builder.add(parseInt(v, 10));
-        } else {
-            builder.add(parseInt(v, 10));
-        }
-    });
-}
-*/
-
 // Display buttons
 for (let button of allButtons) {
     document.body.appendChild(button);
@@ -113,8 +52,4 @@ function constructNumButtons() {
         numButtons.push(numButton);
     }
     return numButtons;
-}
-
-function clearInput() {
-    result.value = "";
 }
